1b90737e87f82ac60e2424c24864a1eb813acba0
Damian Suchy <damian.kropka@gmail.com>
2016-08-31 22:58:53 +0200

    Added base files

---
+++ CitieZ/CitieZ.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace CitieZ
+{
+    [ApiVersion(1, 23)]
+    public class CitieZ : TerrariaPlugin
+    {
+        public CitieZ(Main game) : base(game)
+        {
+        }
+
+        public override string Author => "Renerte";
+        public override string Name => "CitieZ";
+        public override string Description => "Configurable cities system for your TShock server!";
+        public override Version Version { get; } = Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CitieZ
-> Here will be Travis status
+
+|Branch|Status|
+|------|------|
+|master|[![Build Status](https://travis-ci.com/Renerte/CitieZ.svg?token=LoXu7euXY6qjUVn9K8KV&branch=master)](https://travis-ci.com/Renerte/CitieZ)|
+|develop|[![Build Status](https://travis-ci.com/Renerte/CitieZ.svg?token=LoXu7euXY6qjUVn9K8KV&branch=develop)](https://travis-ci.com/Renerte/CitieZ)|
+
 Configurable cities system for your TShock server!
