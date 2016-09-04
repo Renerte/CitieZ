eb8cfa4fb04a992ba2a73e5c157d76a3e5385f52
Damian Suchy <damian.kropka@gmail.com>
2016-09-04 22:12:30 +0200

    Initialization

--- CitieZ/CitieZ.cs
+++ CitieZ/CitieZ.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Data;
+using System.IO;
 using System.Reflection;
 using CitieZ.Db;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
 using Terraria;
 using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
 
 namespace CitieZ
 {
@@ -13,6 +19,8 @@ namespace CitieZ
         {
         }
 
+        public static Config Config { get; private set; }
+        public static IDbConnection Db { get; private set; }
         public static CityManager Cities { get; private set; }
 
         public override string Author => "Renerte";
@@ -20,14 +28,99 @@ namespace CitieZ
         public override string Description => "Configurable cities system for your TShock server!";
         public override Version Version { get; } = Assembly.GetExecutingAssembly().GetName().Version;
 
-        public override void Initialize()
+        protected override void Dispose(bool disposing)
         {
-            ServerApi.Hooks.GameInitialize.Register(this, OnGameInitialize);
+            if (disposing)
+            {
+                GeneralHooks.ReloadEvent -= OnReload;
+                PlayerHooks.PlayerCommand -= OnPlayerCommand;
+
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnGameInitialize);
+                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnGamePostInitialize);
+            }
+            base.Dispose(disposing);
         }
 
-        protected void OnGameInitialize(EventArgs e)
+        public override void Initialize()
         {
-            //TODO: Needs db initialization - put CityManager here afterwards.
+            GeneralHooks.ReloadEvent += OnReload;
+            PlayerHooks.PlayerCommand += OnPlayerCommand;
+
+            ServerApi.Hooks.GameInitialize.Register(this, OnGameInitialize);
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnGamePostInitialize);
+        }
+
+        private void OnPlayerCommand(PlayerCommandEventArgs e)
+        {
+            if (e.Handled || (e.Player == null))
+                return;
+        }
+
+        private void OnReload(ReloadEventArgs e)
+        {
+            var path = Path.Combine(TShock.SavePath, "citiez.json");
+            Config = Config.Read(path);
+            if (!File.Exists(path))
+                Config.Write(path);
+            /*await Cities.ReloadAsync();*/
+            //TODO: CityManager reload.
+            e.Player.SendSuccessMessage("[CitieZ] Reloaded config and homes!");
+        }
+
+        private void OnGameInitialize(EventArgs e)
+        {
+            #region Config
+
+            var path = Path.Combine(TShock.SavePath, "citiez.json");
+            Config = Config.Read(path);
+            if (!File.Exists(path))
+                Config.Write(path);
+
+            #endregion
+
+            #region Database
+
+            if (TShock.Config.StorageType.Equals("mysql", StringComparison.OrdinalIgnoreCase))
+            {
+                if (string.IsNullOrWhiteSpace(Config.MySqlHost) ||
+                    string.IsNullOrWhiteSpace(Config.MySqlDbName))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(
+                        "[CitieZ] MySQL is enabled, but the Essentials+ MySQL Configuration has not been set.");
+                    Console.WriteLine(
+                        "[CitieZ] Please configure your MySQL server information in citiez.json, then restart the server.");
+                    Console.WriteLine("[CitieZ] This plugin will now disable itself...");
+                    Console.ResetColor();
+
+                    Dispose(true);
+
+                    return;
+                }
+
+                var host = Config.MySqlHost.Split(':');
+                Db = new MySqlConnection
+                {
+                    ConnectionString =
+                        $"Server={host[0]}; Port={(host.Length == 1 ? "3306" : host[1])}; Database={Config.MySqlDbName}; Uid={Config.MySqlUsername}; Pwd={Config.MySqlPassword};"
+                };
+            }
+            else if (TShock.Config.StorageType.Equals("sqlite", StringComparison.OrdinalIgnoreCase))
+            {
+                Db = new SqliteConnection(
+                    "uri=file://" + Path.Combine(TShock.SavePath, "citiez.sqlite") + ",Version=3");
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid storage type!");
+            }
+
+            #endregion
+        }
+
+        private void OnGamePostInitialize(EventArgs e)
+        {
+            Cities = new CityManager(Db);
         }
     }
 }
---
+++ CitieZ/Config.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace CitieZ
+{
+    public class Config
+    {
+        public string MySqlDbName = "";
+        public string MySqlHost = "";
+        public string MySqlPassword = "";
+        public string MySqlUsername = "";
+
+        public void Write(string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static Config Read(string path)
+        {
+            return File.Exists(path) ? JsonConvert.DeserializeObject<Config>(File.ReadAllText(path)) : new Config();
+        }
+    }
+}
