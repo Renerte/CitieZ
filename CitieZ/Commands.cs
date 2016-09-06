dd02c47c9d1a9d487ebe3de764b120fd1aaab39f
Damian Suchy <damian.kropka@gmail.com>
2016-09-06 21:52:04 +0200

    Added command functionality and placeholder /city command

---
+++ CitieZ/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+
+namespace CitieZ
+{
+    public static class Commands
+    {
+        public static void City(CommandArgs e)
+        {
+            e.Player.SendInfoMessage("You will be teleported, as soon as Renerte finishes this functionality ;)");
+        }
+    }
+}
